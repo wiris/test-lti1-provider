6bc9393b1ab8828132d296ce779e960707cd152f
daniel marques <dani@wiris.com>
2018-12-07 12:24:10 +0100

    Moved some files

--- membership.php
+++ membership.php
@@ -1,6 +1,6 @@
 <?php
 // https://www.imsglobal.org/specs/ltimemv1p0/specification-3
-include "oauthsign.php";
+include "lib/oauthsign.php";
 
 $a=file_get_contents(dirname(__FILE__)."/outcome.template.xml");
 $url = $_POST["url"];
